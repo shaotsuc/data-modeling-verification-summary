117f6131e1dcc5b458c63466f6f44b15e1edf1ae
Shao-Tsu <shaotsu.chen@gmail.com>
2024-11-27 22:17:44 +0200

    Updated img file

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,11 +23,4 @@ build-db)
     psql -h postgres -d verification -U admin -c "\copy crm.accounts FROM '../data/accounts.csv' DELIMITER ',' CSV"
     psql -h postgres -d verification -U admin -c "\copy prod.clients FROM '../data/clients.csv' DELIMITER ',' CSV"
     psql -h postgres -d verification -U admin -c "\copy prod.verification_sessions FROM '../data/verification_sessions.csv' DELIMITER ',' CSV"
-    psql -h postgres -d verification -U admin -c "\copy prod.documents FROM '../data/documents.csv' DELIMITER ',' CSV"
-  fi
-  ;;
-*)
-  echo "Unknown command: $1"
-  exit 1
-  ;;
-esac
+    psql -h postgres -d verification -U admin -c "\copy prod.documents FROM '../data/documents.csv' DELIMITE
